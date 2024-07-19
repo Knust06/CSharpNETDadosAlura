b7f7ab7e7f4b14d5617197afdccc1dc21c2c2751
Knust06 <lucasknust0605@gmail.com>
2024-07-19 16:39:16 -0300

    Criação de um DAL(Data Acess Layer) Genérico, entendendo os genéricos, biblioteca ORM e como economizar linhas de código com tudo isso.

---
+++ ScreenSound/Banco/DAL.cs
@@ -0,0 +1,42 @@
+﻿using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Banco;
+internal abstract class DAL<T> where T : class
+{
+    protected readonly ScreenSoundContext context;
+
+    protected DAL(ScreenSoundContext context)
+    {
+        this.context = context;
+    }
+
+    public IEnumerable<T> Listar()
+    {
+        return context.Set<T>().ToList();
+    }
+    public void Adicionar(T entidade)
+    {
+        context.Set<T>().Add(entidade);
+        context.SaveChanges();
+    }
+    public void Atualizar(T entidade)
+    {
+        context.Set<T>().Update(entidade);
+        context.SaveChanges();
+    }
+    public void Deletar(T entidade)
+    {
+        context.Set<T>().Remove(entidade);
+        context.SaveChanges();
+    }
+    public T? RecuperarPor(Func<T, bool> condicao)
+    {
+        return context.Set<T>().FirstOrDefault(condicao);
+    }
+
+}
---
+++ ScreenSound/Banco/MusicaDAL.cs
@@ -0,0 +1,15 @@
+﻿using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Banco;
+
+internal class MusicaDAL : DAL<Musica>
+{
+    public MusicaDAL(ScreenSoundContext context) : base(context){  }
+
+}
+
